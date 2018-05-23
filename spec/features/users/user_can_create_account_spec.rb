bb17e948b2a03789ac242ec594a488ee65eca7aa
Evan Wheeler <anon0mys87@gmail.com>
2018-05-23 16:05:07 -0600

    Users create action and redirect to dashboard:

--- spec/features/users/user_can_create_account_spec.rb
+++ spec/features/users/user_can_create_account_spec.rb
@@ -7,7 +7,7 @@ feature 'A User can sign up' do
     fill_in 'user[name]', with: 'Test'
     fill_in 'user[email]', with: 'test@mail.com'
     fill_in 'user[password]', with: 'password'
-    fill_in 'user[password-confirmation]', with: 'password'
+    fill_in 'user[password_confirmation]', with: 'password'
 
     click_on 'Sign Up'
 
