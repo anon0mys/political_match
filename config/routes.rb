9d06ee1755428909fd8df2f6b38538bdc1eaa45a
Evan Wheeler <anon0mys87@gmail.com>
2018-05-23 15:10:40 -0600

    Basic content for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  get '/sign-up', to: 'users#new', as: :sign_up
+  get '/login', to: 'sessions#new', as: :login
 end
