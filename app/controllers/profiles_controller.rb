f7da45db45402b8a6669e27c12acd27e0c7e9a42
Evan Wheeler <anon0mys87@gmail.com>
2018-05-28 22:15:53 -0600

    Begins building feature for creating a political profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,12 +6,17 @@ class ProfilesController < ApplicationController
   def create
     profile_builder = ProfileBuilder.new(profile_params)
     user = User.find(params[:user_id])
-    profile = user.profile.new(profile_builder.results)
+    profile = user.build_profile(profile_builder.results)
     if profile.save
       redirect_to dashboard_path
     else
       flash[:error] = 'Your profile was not saved'
       redirect_to new_user_profile_path(user)
     end
+  end
+
+  private
+
+  def profile_params
   end
 end
--- spec/features/profiles/user_can_create_a_profile_spec.rb
+++ spec/features/profiles/user_can_create_a_profile_spec.rb
@@ -7,14 +7,14 @@ feature 'A logged in User' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit new_user_profile_path(user)
 
-    select 'Democrat', from: 'question_1'
+    select 'This is a liberal response', from: 'question_1'
 
     click_on 'Submit'
 
     expect(current_path).to eq('/dashboard')
     within '.profile' do
-      expect(page).to have_content('You fall most in the:')
-      expect(page).to have_content()
+      expect(page).to have_content('You agree most with:')
+      expect(page).to have_content('Solid Liberals')
     end
   end
 end
