851de9531f05456366dda19694bf975733cbd7e6
Evan Wheeler <anon0mys87@gmail.com>
2018-05-29 00:08:41 -0600

    Added second selection to survey, updated profile spec to reflect changes to survey

--- spec/features/profiles/user_can_create_a_profile_spec.rb
+++ spec/features/profiles/user_can_create_a_profile_spec.rb
@@ -5,9 +5,14 @@ feature 'A logged in User' do
     user = create(:user)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    stub_request(:post, 'https://apiv2.indico.io/political/batch')
+      .to_return(body: './spec/fixtures/json/liberal_survey.json')
+
     visit new_user_profile_path(user)
 
     select 'This is a liberal response', from: 'question_1'
+    select 'This is a liberal response', from: 'question_2'
 
     click_on 'Submit'
 
