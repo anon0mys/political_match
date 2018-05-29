98a95cc627b8af30191ae08ec41ce7090f82d405
Evan Wheeler <anon0mys87@gmail.com>
2018-05-29 09:08:36 -0600

    Fix to invalid login in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       redirect_to dashboard_path
     else
       flash[:error] = 'Invalid username or password'
-      render login
+      render 'sessions/new'
     end
   end
 
