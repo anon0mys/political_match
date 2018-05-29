0dd02e43c742e356800cb855948e4bfda9ff2bf8
Evan Wheeler <anon0mys87@gmail.com>
2018-05-28 22:07:33 -0600

    Adds indico api service for political text analysis

---
+++ app/services/indico_service.rb
@@ -0,0 +1,27 @@
+class IndicoService
+  def initialize(responses)
+    @conn = Faraday.new(url: 'https://apiv2.indico.io/political/batch')
+    @responses = responses
+  end
+
+  def analyze
+    response = make_request
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+  attr_reader :responses
+
+  def request_body
+    binding.pry
+    results = { data: @responses.values, threshold: 0.25 }
+    results.to_json
+  end
+
+  def make_request
+    @conn.post do |req|
+      req.headers['X-Apikey'] = ENV['INDICO_API_KEY']
+      req.body = request_body
+    end
+  end
+end
---
+++ spec/services/indico_service_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe IndicoService do
+  context 'political analysis' do
+    it 'can determine liberal text' do
+      survey = {
+        question_1: 'This is a liberal response',
+        question_2: 'This is a liberal response'
+      }
+
+      service = IndicoService.new(survey)
+
+      response = service.analyze
+      results = response[:results]
+
+      expect(results).to be_an Array
+      expect(results.first).to be_a Hash
+      expect(results.first.keys.first).to eq(:Liberal)
+    end
+
+    it 'can determine conservative text' do
+      survey = {
+        question_1: 'This is a conservative response',
+        question_2: 'This is a conservative response'
+      }
+
+      service = IndicoService.new(survey)
+
+      response = service.analyze
+      results = response[:results]
+
+      expect(results).to be_an Array
+      expect(results.first).to be_a Hash
+      expect(results.first.keys.first).to eq(:Conservative)
+    end
+  end
+end
