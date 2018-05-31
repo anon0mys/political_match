19e79a5e28b9d69877d1b151fcd1035310896a5b
Evan Wheeler <anon0mys87@gmail.com>
2018-05-31 13:36:33 -0600

    Adds politician factory

---
+++ spec/factories/politicians.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :politician do
+    name "MyString"
+    role "MyString"
+    gender "MyString"
+    party "MyString"
+    twitter_id "MyString"
+    facebook_account "MyString"
+    next_election "MyString"
+  end
+end
