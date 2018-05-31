c4b056f68a68599edb3b41b2b95142d4a1e0dfb6
Evan Wheeler <anon0mys87@gmail.com>
2018-05-31 14:23:22 -0600

    Refactors politician attributes in twitter job spec

--- spec/jobs/twitter_getter_job_spec.rb
+++ spec/jobs/twitter_getter_job_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 describe TwitterGetterJob do
   context '#perform' do
+    let(:attributes) {
+      {
+        name: 'Test',
+        role: 'test',
+        gender: 'test',
+        party: 'test',
+        twitter_id: 'RepDianaDeGette',
+        facebook_account: 'test',
+        next_election: 'test',
+        profile: nil
+      }
+    }
+
     it 'should update all politician profiles' do
-      before_update = Politician.create(name: 'Test',
-                                        role: 'test',
-                                        gender: 'test',
-                                        party: 'test',
-                                        twitter_id: 'RepDianaDeGette',
-                                        facebook_account: 'test',
-                                        next_election: 'test',
-                                        profile: nil)
+      before_update = Politician.create(attributes)
 
       expect(before_update.profile).to be nil
 
