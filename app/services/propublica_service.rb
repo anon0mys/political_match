b971eb08b59cecb483e0a12caea4642dc632f4e0
Evan Wheeler <anon0mys87@gmail.com>
2018-06-02 02:12:42 -0600

    Changes to the propublica service to allow for dynamic url params

--- app/services/propublica_service.rb
+++ app/services/propublica_service.rb
@@ -1,12 +1,17 @@
 class PropublicaService
-  def initialize(congress, chamber)
+  def initialize(url_params)
     @conn = Faraday.new(url: 'https://api.propublica.org/')
-    @url = "congress/v1/#{congress}/#{chamber}/members.json"
+    @url = build_url(url_params)
   end
 
   def get_members
     response = JSON.parse(make_request.body, symbolize_names: true)
     parse_members(response[:results].first[:members])
+  end
+
+  def get_member
+    response = JSON.parse(make_request.body, symbolize_names: true)
+    response[:results].first
   end
 
   private
@@ -19,10 +24,19 @@ class PropublicaService
   end
 
   def parse_members(members)
-    keep = %i[first_name last_name title state party twitter_account govtrack_id]
+    keep = %i[first_name last_name title state party twitter_account propublica_id]
     members.reduce([]) do |results, member|
+      member[:propublica_id] = member[:id]
       results << member.delete_if{|key, value| !keep.include?(key)}
       results
+    end
+  end
+
+  def build_url(url_params)
+    if url_params.keys.include?(:member_id)
+      "congress/v1/members/#{url_params[:member_id]}.json"
+    else
+      "congress/v1/#{url_params[:congress]}/#{url_params[:chamber]}/members.json"
     end
   end
 end
