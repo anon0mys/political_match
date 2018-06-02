eb703b15e35224d3c4cb7d55d92bd55e30c53157
Evan Wheeler <anon0mys87@gmail.com>
2018-06-01 22:27:46 -0600

    Updates twitter job to use VCR

--- spec/jobs/twitter_getter_job_spec.rb
+++ spec/jobs/twitter_getter_job_spec.rb
@@ -3,15 +3,22 @@ require 'rails_helper'
 describe TwitterGetterJob do
   context '#perform' do
     it 'should update all politician profiles' do
-      before_update = create(:politician)
+      VCR.use_cassette('twitter-list-service') do
+        before_update = create(:politician)
 
-      expect(before_update.profile).to be nil
+        expect(before_update.profile).to be nil
 
-      TwitterGetterJob.perform_now
+        indico_response = File.read('./spec/fixtures/json/liberal_survey.json')
 
-      after_update = Politician.find(1)
+        stub_request(:post, 'https://apiv2.indico.io/political/batch')
+          .to_return(body: indico_response)
 
-      expect(after_update.profile).to be_a Profile
+        TwitterGetterJob.perform_now
+
+        after_update = Politician.find(1)
+
+        expect(after_update.profile).to be_a Profile
+      end
     end
   end
 end
