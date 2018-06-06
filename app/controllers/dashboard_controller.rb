c35dd8919f4d9a1c69f166e3ac0b6ea942dd9f4e
Evan Wheeler <anon0mys87@gmail.com>
2018-06-06 00:13:14 -0600

    Adds presenter to profile summary view and dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   def show
     @user = current_user
     unless @user.profile.nil?
-      @profile = JSON.parse(@user.profile.overall, symbolize_names: true)
+      @profile = ProfilePresenter.new(@user, @user.profile)
     end
   end
 end
