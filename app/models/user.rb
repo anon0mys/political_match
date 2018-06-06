bebadb17f8d5a28480dea53ae69da3622b97be74
Evan Wheeler <anon0mys87@gmail.com>
2018-06-06 08:08:05 -0600

    Modified user to not validate email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :email, :password_digest
+  validates_presence_of :name, :password_digest
   validates :password_digest, confirmation: { case_sensitive: true }
   has_secure_password
   has_one :profile, as: :owner
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe User do
   describe 'validations' do
     it { should validate_presence_of :name }
-    it { should validate_presence_of :email }
     it { should validate_presence_of :password_digest }
     it { should validate_confirmation_of :password_digest }
   end
