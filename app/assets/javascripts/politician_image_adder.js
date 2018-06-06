b4e5d09479dd0bbad23408161ce51da7ebff5d22
Evan Wheeler <anon0mys87@gmail.com>
2018-06-06 11:59:27 -0600

    Adds images to politician cards on index

---
+++ app/assets/javascripts/politician_image_adder.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+  fetchPoliticianPictures();
+});
+
+const fetchPoliticianPictures = () => {
+  let spans = $('.politician-image-id')
+  let list = Object.values(spans)
+  let politicianIds = list.map(element => {
+    if(element.innerText) {
+      return element.innerText
+    };
+  })
+
+  politicianIds.forEach(function(id) {
+    if(typeof id === 'undefined') return;
+    fetch(`https://theunitedstates.io/images/congress/225x275/${id}.jpg`)
+      .then(result => {
+        appendImage(result.url)
+      })
+      .catch(error => console.error(error))
+  })
+};
+
+const appendImage = (url) => {
+  let string = url.split('.')[1].split('/')
+  let divId = string[string.length - 1]
+  $(`#${divId}`).append(`<image src=${url}></image>`)
+}
