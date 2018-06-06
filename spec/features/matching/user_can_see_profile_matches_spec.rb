aeba795f69be80d92c8c34f6b228be508931de37
Evan Wheeler <anon0mys87@gmail.com>
2018-06-05 20:42:58 -0600

    Adds best matches to index view

--- spec/features/matching/user_can_see_profile_matches_spec.rb
+++ spec/features/matching/user_can_see_profile_matches_spec.rb
@@ -3,12 +3,18 @@ require 'rails_helper'
 feature 'A user with a profile' do
   context 'can visit /matches' do
     before(:each) do
-      user = create(:user_with_profile)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      @user = create(:user_with_profile)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 
     scenario 'and see the best matched politicians' do
+      preferred_party = @user.profile.preferred_party
+      political_type = @user.profile.political_type
+
       create_list(:politician_with_profile, 20)
+      4.times do
+        create(:politician, profile: create(:profile, political_type: political_type, preferred_party: preferred_party))
+      end
 
       visit matches_path
 
