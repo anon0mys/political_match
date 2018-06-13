fff7f0efcf2fb07acf2951cdef43d10a80a5be1f
Evan Wheeler <anon0mys87@gmail.com>
2018-06-13 13:39:05 -0600

    Gauge chart only in profile div

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -14,12 +14,6 @@ const fetchPoliticalMatches = () => {
             <a href="/politicians/${element.model.id}">${element.model.title} ${element.model.first_name} ${element.model.last_name}</a>
             <li class="type">Political Type: ${element.profile.political_type}</li>
             <li class="preferred_party">Preferred Party: ${element.profile.preferred_party}</li>
-            <div class="gauge-chart" id="gauge_chart_small">
-              <script>
-                var authority_rating = ${element.authority_rating};
-                var social_rating = ${element.social_rating};
-              </script>
-            </div>
           </ul>`);
       })
     });
